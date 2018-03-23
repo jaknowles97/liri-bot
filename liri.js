7beffbbc397fb58bed0567b7d55d3def57a058dd
Andrew <jaknowles1997@gmail.com>
2018-03-22 19:45:11 -0700

    omdb is fully functioning

--- liri.js
+++ liri.js
@@ -5,7 +5,7 @@ var Spotify = require('node-spotify-api');
 var request = require("request");
 
 // getting user's command
-var userParam = process.argv.splice(2).join(" ").split("- ");
+var userParam = process.argv.splice(2).join(" ").split("-");
 
 // API Functions
 var spotifyReq = function(song) {
@@ -17,13 +17,13 @@ var spotifyReq = function(song) {
       spotify
         .search({ type: 'track', query: song })
         .then(function(response) {
-            var shortcut = response.tracks.items[0];
+            var results = response.tracks.items[0]; 
           console.log(
               "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n" +
-              " Artist(s): " + shortcut.artists[0].name + "\n" +
-              " Song: " + shortcut.name + "\n" +
-              " Album: " + shortcut.album.name + " \n" +
-              " song preview: " + shortcut.preview_url + "\n" +
+              " Artist(s): " + results.artists[0].name + "\n" +
+              " Song: " + results.name + "\n" +
+              " Album: " + results.album.name + " \n" +
+              " song preview: " + results.preview_url + "\n" +
               "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
             );
         })
@@ -32,20 +32,51 @@ var spotifyReq = function(song) {
         });
 }
 
-var omdbReq = function() {
+var omdbReq = function(movie) {
     var apiKey = "apikey=trilogy";
     var queryUrl = "http://www.omdbapi.com/?" + apiKey;
+    var searchParam = "&t=" + movie.split(" ").join("+");
 
     var request = require('request');
-    request(queryUrl + "&s='the+room", function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body);
+    request(queryUrl + searchParam, function (error, response, body) {
+        console.log('error:', error); 
+        console.log('statusCode:', response.statusCode);
+        var movie =JSON.parse(body, null , 2); // prettify in case i want to print the whole obj in console for testing purposes
+
+        console.log(
+            "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n" +
+            " Title: " + movie.Title + "\n\n" +
+            " Year Realeased: " + movie.Year + "\n\n" +
+            " IMBD Rating: " + movie.imdbRating + " \n\n" +
+            " Rotten Tomatoes: " + movie.Ratings[1].Value + "\n\n" +
+            " Country Produced: " + movie.Country + "\n\n" +
+            " Native Lang: " + movie.Language + "\n\n" +
+            " Plot: \n  " + movie.Plot + "   \n\n" +
+            " Actors: " + movie.Actors + "\n\n" +
+            "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+        );
     });
 }
 
-if(userParam) {
-    if(userParam[0] === "spot this song") {
-        spotifyReq(userParam[1]);
+    // Based on what command the user gave to liri, do..
+    switch(userParam[0]) {
+        case "spot this song":
+            switch(userParam[1]) {
+                case undefined || '': spotifyReq("The Sign Ace of Base"); break;
+                default: spotifyReq(userParam[1]);
+            }
+        break;
+        case "movie this":
+            switch(userParam[1]) {
+                case undefined || '': omdbReq("Mr.Nobody"); break;
+                default: omdbReq(userParam[1].trim());
+            }
+        break;
+        case "my tweets":
+
+        break;
+        default:
+            console.log("give liri a cmd !");
     }
-}
+
+
